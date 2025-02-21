f082e834e18f1162d899ef07370c6f366bef2ba8
Ghulam Mustafa <157616503+G-Mustafa1@users.noreply.github.com>
2025-02-21 12:44:32 +0500

    Update dashbord.js

--- dist/dashbord.js
+++ dist/dashbord.js
@@ -2,6 +2,8 @@ import { auth, signOut } from "./firebase.js";
 console.log('sg', signOut);
 const nav_nam = document.getElementById('nav-nam');
 const sing_out = document.getElementById('sing-out');
+const hide = document.getElementById('hide');
+const loader = document.getElementById('loader');
 nav_nam.textContent = "Hello' Welcome !";
 sing_out.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -25,3 +27,13 @@ sing_out.addEventListener('submit', (e) => {
         });
     });
 });
+
+function loadform() {
+    setTimeout(() => {
+        loader.style.display = 'none';
+        hide.style.display = 'block';
+    }, 1000);
+}
+window.addEventListener('load', loadform);
+
+
